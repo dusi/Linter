64bb46421226d8c57e5c14e78ec668622db623ef
Pavel Dusatko <pavel@fitbod.me>
2025-06-03 22:37:51 -0700

    Fix

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -5,6 +5,10 @@ let danger = Danger()
 
 let changedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 
+if changedFiles.count - danger.git.deletedFiles.count > 300 {
+    warn("BIG PR - Try to keep changes smaller if you can")
+}
+
 SwiftLint.lint(
     .files(changedFiles),
     inline: true,
